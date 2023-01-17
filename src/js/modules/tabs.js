272e725c6caa0aa2682a9c662a484d273eaa8354
Vasyl Vlasiuk <vlasiuk_vasyl@ukr.net>
2023-01-18 00:01:44 +0200

    implementation of tabs

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,36 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass, display = 'block') => {
+  const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+
+  function hideTabContent() {
+    content.forEach(item => item.style.display = 'none')
+    tab.forEach(item => item.classList.remove(activeClass));
+  }
+
+  function showTabContent(i = 0) {
+    content[i].style.display = display;
+    tab[i].classList.add(activeClass);
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  header.addEventListener('click', (e) => {
+    const target = e.target;
+    tabSelector = tabSelector.replace(/\./, "");
+  
+    if (target && target.classList.contains(tabSelector) ||
+    target.parentNode.classList.contains(tabSelector)) {
+    
+      tab.forEach((item, i) => {
+        if (target == item || target.parentNode == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+};
+
+export default tabs;
